875807e519ec4448cf3dd05fac2accb1221bac5d
Dan Adiletta <dan.adiletta@gmail.com>
2024-05-16 17:20:42 -0400

    follow cone init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/FollowCone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/FollowCone.java
@@ -6,10 +6,12 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 
 public class FollowCone extends CommandBase {
     private OpenCvCamera webcam;
+    private Sagan robot;
     private Pipeline pipeline;
 
-    public FollowCone(Pipeline pipeline) {
-        this.pipeline = pipeline;
+    public FollowCone(Sagan robot) {
+        this.robot = robot;
+        this.pipeline = robot.pipeline;
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
@@ -20,7 +22,19 @@ public class FollowCone extends CommandBase {
 
     @Override
     public void execute() {
-        // Here you can add the logic to follow the cone based on the output of the pipeline
+        // Process the image and get the Mat
+        Mat processedImage = pipeline.processImage();
+
+        // Calculate the distance and direction to the cone
+        double distance = calculateDistanceToCone(processedImage);
+        double direction = calculateDirectionToCone(processedImage);
+
+        // Calculate the speed and direction for the robot to drive
+        double speed = calculateSpeed(distance);
+        double driveDirection = calculateDriveDirection(direction);
+
+        // Drive the robot
+        robot.drive.drive(0, 0, 0 ,0);
     }
 
     @Override
@@ -32,5 +46,25 @@ public class FollowCone extends CommandBase {
     public boolean isFinished() {
         // Here you can add the condition that determines when the command should end
         return false;
+    }
+
+    private double calculateDistanceToCone(Mat processedImage) {
+        // Here you would add the logic to calculate the distance to the cone based on the processed image
+        return 0;
+    }
+    
+    private double calculateDirectionToCone(Mat processedImage) {
+        // Here you would add the logic to calculate the direction to the cone based on the processed image
+        return 0;
+    }
+    
+    private double calculateSpeed(double distance) {
+        // Here you would add the logic to calculate the speed based on the distance
+        return 0;
+    }
+    
+    private double calculateDriveDirection(double direction) {
+        // Here you would add the logic to calculate the drive direction based on the direction
+        return 0;
     }
 }
